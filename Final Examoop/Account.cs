f58b863f1d288b7b54a959286a49e4c775aad6ee
Mahnoor-Akhter <60778404+Mahnoor-Akhter@users.noreply.github.com>
2021-01-11 10:09:27 +0000

    make abstruct Account class

---
+++ Final Examoop/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Examoop
+{
+	public abstract class Account
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public decimal Balance { get; private set; }
+
+		public int interestDate { get; set; }
+	}
+}
